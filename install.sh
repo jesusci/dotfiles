e0906148c0b2663973d3cb05027034a4413e3d86
Jesus <jesus.cristina.iglesias@gmail.com>
2019-04-11 22:51:47 +0200

    Refactor install script

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+##### Colours
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 REPO_HOME="$HOME/dotfiles"
 REPO_PWD=$(pwd)
 
@@ -6,8 +12,20 @@ wvim=$(which vim)
 wtmux=$(which tmux)
 wnvim=$(which nvim)
 
+function check_diff()
+{
+    result=$(diff $1 $2)
+
+    if [ $? -eq 0 ];then
+        echo -e "${GREEN}Current local configuration and repo configuration are the same${NC}"
+    else
+        echo -e "${RED}Current local configuration and repo configuration are different${NC}"
+    fi
+}
+
+
 if [ $REPO_HOME != $REPO_PWD ]; then
-    echo "To install dotfiles this repo must be in $REPO_HOME"
+    echo -e "${RED}To install dotfiles this repo must be in $REPO_HOME${NC}"
     exit 0
 fi
 
@@ -16,18 +34,12 @@ fi
 # ====================================================
 
 #### Check if vim is installed
-if [ -n $wvim ]; then
+if [ ! -z $wvim ]; then
+    echo ""
     echo "Checking vim configuration..."
     if [ -e $HOME/.vimrc ]; then
         echo -n "  There is a current vim configuration: "
-        result=$(diff $HOME/.vimrc $REPO_HOME/vim/.vimrc)
-
-        if [ $? -eq 0 ];then
-            echo "Current local configuration and repo configuration are the same"
-        else
-            echo "Current local configuration and repo configuration are different"
-        fi
-
+        check_diff $HOME/.vimrc $REPO_HOME/vim/.vimrc
     else
         ln -s $REPO_HOME/vim/.vimrc     $HOME/.vimrc
         echo "---- Vim configuration installed ----"
@@ -38,18 +50,12 @@ fi
 #               TMUX configuration
 # ====================================================
 #### Check if tmux is installed
-if [ -n $wtmux ]; then
+if [ ! -z $wtmux ]; then
+    echo ""
     echo "Checking tmux configuration..."
     if [ -e $HOME/.tmux.conf ]; then
         echo -n "  There is a current tmux configuration: "
-        result=$(diff $HOME/.tmux.conf $REPO_HOME/tmux/.tmux.conf)
-
-        if [ $? -eq 0 ];then
-            echo "Current local configuration and repo configuration are the same"
-        else
-            echo "Current local configuration and repo configuration are different"
-        fi
-
+        check_diff $HOME/.tmux.conf $REPO_HOME/tmux/.tmux.conf
     else
         ln -s $REPO_HOME/tmux/.tmux.conf     $HOME/.tmux.conf
         echo "---- TMUX configuration installed ----"
@@ -60,20 +66,16 @@ fi
 #               NeoVim configuration
 # ====================================================
 #### Check if tmux is installed
-if [ -n $wnvim ]; then
+if [ ! -z $wnvim ]; then
+    echo ""
     echo "Checking NeoVim configuration..."
     if [ -e $HOME/.config/nvim/init.vim ]; then
         echo -n "  There is a current Neovim configuration: "
-        result=$(diff $HOME/.config/nvim/init.vim $REPO_HOME/neovim/init.vim)
-
-        if [ $? -eq 0 ];then
-            echo "Current local configuration and repo configuration are the same"
-        else
-            echo "Current local configuration and repo configuration are different"
-        fi
-
+        check_diff $HOME/.config/nvim/init.vim $REPO_HOME/neovim/init.vim
     else
         ln -s $REPO_HOME/neovim/init.vim     $HOME/.config/nvim/init.vim
         echo "---- NEOVIM configuration installed ----"
     fi
 fi
+
+echo ""
