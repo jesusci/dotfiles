5a1b92982ce5cdd859c5ec3aecb5e9a70806cb2a
jesusci <jesus.cristina.iglesias@gmail.com>
2021-08-28 21:20:14 +0200

    Recover plugins code

--- utils/install_vim_submodules.sh
+++ utils/install_vim_submodules.sh
@@ -2,9 +2,18 @@
 
 REPO_PWD=$(pwd)
 
+source $REPO_PWD/utils/print_options.sh
 function install_youcomplete_me()
 {
-    $REPO_PWD/vim/pack/youcompleteme/start/YouCompleteMe/install.sh
+    clear
+    print_warning "Want to install YouCompleteMe plugin? [y/N]"
+    read user_ask
+
+    if [ "$user_ask" == "yes" ] || [ "$user_ask" == "y" ] || [ "$user_ask" == "Y" ] ; then
+        $REPO_PWD/vim/pack/youcompleteme/start/YouCompleteMe/install.sh
+    else
+        print_warning "YouCompleteMe plugin not installed"
+    fi
 }
 
 git submodule update --init --recursive
